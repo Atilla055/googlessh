1c24e0b87825172e2cc03a0194f1e8dc2a8058db
Atilla055 <78541725+Atilla055@users.noreply.github.com>
2023-11-04 19:40:16 -0400

    Update Atilla.py

--- Atilla.py
+++ Atilla.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 # Firefox profilinizin dizin yolunu ve profil adını belirtin
 profile_path = "/root/.mozilla/firefox/dlr2maa5.default-release"
@@ -10,19 +9,23 @@ firefox_profile = webdriver.FirefoxProfile(profile_path)
 # WebDriver'ı başlatırken profilinizi kullanın
 options = webdriver.FirefoxOptions()
 options.profile = firefox_profile
-driver = webdriver.Firefox(options=options)
+
+# İlk sayfa için yeni bir WebDriver oluşturun
+driver1 = webdriver.Firefox(options=options)
 
 # İlk sayfaya gidin
-driver.get("https://addons.mozilla.org/en-US/firefox/addon/webgl-fingerprint-defender/")
+driver1.get("https://addons.mozilla.org/en-US/firefox/addon/webgl-fingerprint-defender/")
 
 # Sayfanın yüklenmesini beklemek için gerekli bir bekleme süresi ekleyin
-driver.implicitly_wait(10)  # Örnekte 10 saniye bekleme süresi kullanıldı, gerektiği gibi ayarlayabilirsiniz.
+driver1.implicitly_wait(10)
 
-# "Add to Firefox" düğmesine tıklayın
-add_to_firefox_button = driver.find_element(By.ID, "addon-add-button")
-add_to_firefox_button.click()
+# İkinci sayfa için yeni bir WebDriver oluşturun
+driver2 = webdriver.Firefox(options=options)
+
+# İkinci sayfaya gidin
+driver2.get("https://addons.mozilla.org/en-US/firefox/addon/canvas-fingerprint-defender/")
 
 # Sayfanın yüklenmesini beklemek için gerekli bir bekleme süresi ekleyin
-driver.implicitly_wait(10)  # Örnekte 10 saniye bekleme süresi kullanıldı, gerektiği gibi ayarlayabilirsiniz.
+driver2.implicitly_wait(10)
 
-# WebDriver'ı kapatın
+# WebDriver'ları kapatı
