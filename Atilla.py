3375d63331945ea21da55eaf4e874011c6729f42
Atilla055 <78541725+Atilla055@users.noreply.github.com>
2023-11-04 19:33:27 -0400

    Update Atilla.py

--- Atilla.py
+++ Atilla.py
@@ -1,6 +1,5 @@
-
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Firefox profilinizin dizin yolunu ve profil adını belirtin
 profile_path = "/root/.mozilla/firefox/dlr2maa5.default-release"
@@ -19,12 +18,11 @@ driver.get("https://addons.mozilla.org/en-US/firefox/addon/webgl-fingerprint-def
 # Sayfanın yüklenmesini beklemek için gerekli bir bekleme süresi ekleyin
 driver.implicitly_wait(10)  # Örnekte 10 saniye bekleme süresi kullanıldı, gerektiği gibi ayarlayabilirsiniz.
 
-# İkinci sayfaya gidin
-driver.get("https://addons.mozilla.org/en-US/firefox/addon/canvas-fingerprint-defender/")
+# "Add to Firefox" düğmesine tıklayın
+add_to_firefox_button = driver.find_element(By.ID, "addon-add-button")
+add_to_firefox_button.click()
 
 # Sayfanın yüklenmesini beklemek için gerekli bir bekleme süresi ekleyin
 driver.implicitly_wait(10)  # Örnekte 10 saniye bekleme süresi kullanıldı, gerektiği gibi ayarlayabilirsiniz.
 
 # WebDriver'ı kapatın
-driver.quit()
-
