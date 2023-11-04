57078bb164517ee922a2eb0b7d5e011304ee22e3
Atilla055 <78541725+Atilla055@users.noreply.github.com>
2023-11-04 18:26:56 -0400

    Update Atilla.py

--- Atilla.py
+++ Atilla.py
@@ -18,9 +18,9 @@ driver.get("https://www.youtube.com/@mahperest/videos")
 time.sleep(10)  # Sayfanın yüklenmesini beklemek için birkaç saniye bekleyin
 
 # Sayfada "Pubg" isimli videoları açın
-video_elements = driver.find_elements(By.XPATH, "//a[contains(@title, 'Pubg')]")
-for video_element in video_elements:
-    video_url = video_element.get_attribute("href")
-    driver.execute_script("window.open(arguments[0], '_blank');", video_url)
+## video_elements = driver.find_elements(By.XPATH, "//a[contains(@title, 'Pubg')]")
+## for video_element in video_elements:
+  ##  video_url = video_element.get_attribute("href")
+   ## driver.execute_script("window.open(arguments[0], '_blank');", video_url)
 
 # Selenium çalışmasını kapatın
